7df7639d02323c7d8c1c468456b0707e0758d063
Eren Sertkaya <eren.sertkaya.es@gmail.com>
2018-11-28 15:21:38 +0300

    first commit

---
+++ nodejs-practice.js
@@ -0,0 +1,21 @@
+// Js Giriş
+/*
+console.log("hello world");
+var data = "eren sertkaya";
+console.log(data);
+console.log(data +" "+1998);
+*/
+
+// Aritmetik Operatörler
+
+var number = 2;
+console.log('number'+2);
+console.log("number"+2);
+console.log(number + 2);
+console.log(number * 2);
+console.log(number / 2);
+console.log(number % 2);
+console.log(++number);
+
+
+
