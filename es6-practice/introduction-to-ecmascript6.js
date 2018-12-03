0d7083d53bfca9526e4cb74f016c26aa4f346bd9
Eren Sertkaya <eren.sertkaya.es@gmail.com>
2018-12-03 14:33:02 +0300

    es6 practices completed

--- es6-practice/introduction-to-ecmascript6.js
+++ es6-practice/introduction-to-ecmascript6.js
@@ -138,63 +138,6 @@ hellFunction().then((data) => {
     console.log(z);
 })
 */
-let userId;
-const user = {id: 10, name: 'eren'};
-const friends = [{id: 11, name: 'samet'}, {id: 12, name: 'yasin'}];
-
-const getUser = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(user);
-        }, 1000);
-    });
-};
-
-const getFriends = (userId) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(friends);
-        }, 500)
-    });
-};
-
-
-/*
-
-//callback hell
-getUser().then((x) => {
-    userId = x.id;
-    getFriends(userId).then((y) => {
-        console.log(y);
-        console.log(x);
-    })
-});
-
-*/
-
-/*
-//Promise Chain Instance
-
-getUser()
-    .then((x) => {
-        return x.id;
-    })
-    .then((userId) => {
-        getFriends(userId)
-            .then((friends) => {
-                console.log(friends);
-            })
-    })
-*/
-//Promise Chain Instance 2
-
-getUser()
-    .then((x) => {
-        return getFriends(x.id);
-    })
-    .then((friends) => {
-        console.log(friends);
-    })
 
 
 
